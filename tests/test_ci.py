6a44597ee0ca2e07012c86d4ea8bca6612cd58b5
Oleksandr Demeniuk <demeniuk@gmail.com>
2020-08-03 14:33:30 -0500

    test  updates

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,7 +1,11 @@
 
-def test_amazon(py):
+def test_amazon_fail(py):
     py.visit('https://amazon.com')
     assert py.contains('0Try Prime')
 
 
+
+def test_amazon_success(py):
+    py.visit('https://amazon.com')
+    assert py.contains('Try Prime')
 
